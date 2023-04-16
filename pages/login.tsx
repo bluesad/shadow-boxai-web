be23a76912a39333c262c33b639d680b921e93ea
ishida <webmaster@joe1cafe.com>
2023-04-16 14:36:39 +0800

    feat(add new intro page)

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,10 @@
 import { NextPage } from 'next';
 
-// import { useNotLoggedIn } from '@/common/hooks/auth';
+import { useNotLoggedIn } from '@/common/hooks/auth';
 import Login from '@/modules/login/components/Login';
 
-const LoginPage: NextPage = () => (
-  // useNotLoggedIn();
-
-  <Login />
-);
+const LoginPage: NextPage = () => {
+  useNotLoggedIn();
+  return <Login />;
+};
 export default LoginPage;
